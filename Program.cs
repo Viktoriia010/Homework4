7e60861e27383d2da41b9de9f163c19d841ac6e4
Viktoria Koshman <koshvikt10@gmail.com>
2025-12-22 13:09:46 +0200

    added first task

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework4;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        TemperatureArray t = new TemperatureArray();
+        t.Show();
+        t.InputTemp();
+        t.Show();
+        Console.WriteLine($"average: {t.TempAverage()}");
+    }
+}
---
+++ TemperatureArray.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4;
+
+internal class TemperatureArray
+{
+    public double Monday { get; set; }
+    public double Tuesday { get; set; }
+    public double Wednesday { get; set; }
+    public double Thursday { get; set; }
+    public double Friday { get; set; }
+    public double Saturday { get; set; }
+    public double Sunday { get; set; }
+
+    public double this[int index]
+    {
+        get
+        {
+            return index switch
+            {
+                0 => Monday,
+                1 => Tuesday,
+                2 => Wednesday,
+                3 => Thursday,
+                4 => Friday,
+                5 => Saturday,
+                6 => Sunday,
+                _ => throw new Exception("Некорректный индекс! " + index)
+            };
+         
+        }
+        set
+        {
+            switch (index)
+            {
+                case 0: Monday = value; break;
+                case 1: Tuesday = value; break;
+                case 2: Wednesday = value; break;
+                case 3: Thursday = value; break;
+                case 4: Friday = value; break;
+                case 5: Saturday = value; break;
+                case 6: Sunday = value; break;
+                default:
+                    throw new Exception("Некорректный индекс! " + index);
+            }
+        }
+    }
+
+    public void Show()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            Console.WriteLine($"День {i}: {this[i]}");
+        }
+    }
+
+    public void InputTemp()
+    {
+        Console.WriteLine("Enter the temperature for the week: ");
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.Write($"{i}: ");
+            this[i] = double.Parse(Console.ReadLine());
+        }
+        Console.WriteLine();
+    }
+
+    public double TempAverage()
+    {
+        return (Monday + Tuesday + Wednesday + Thursday +
+                Friday + Saturday + Sunday) / 7;
+    }
+}

d85bc18242e852ce97ac1ac8cc67e77353de01b1
Viktoria Koshman <koshvikt10@gmail.com>
2025-12-23 17:56:13 +0200

    added second task

---
+++ Product.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4;
+
+internal class Product
+{
+    public string Name { get; set; }
+    private int amount;
+    private int price;
+
+    public int Amount
+    {
+        get { return amount; }
+        set
+        {
+            if(value >= 0)
+            {
+                amount = value;
+            }
+            else
+            {
+                throw new Exception("Value cannot be negative");
+            }
+        }
+    }
+
+    public int Price
+    {
+        get { return price; }
+        set
+        {
+            if (value >= 0)
+            {
+                price = value;
+            }
+            else
+            {
+                throw new Exception("Value cannot be negative");
+            }
+        }
+    }
+
+    public Product(string name, int a, int p)
+    {
+        Name = name;
+        Amount = a; 
+        Price = p;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Name: {Name} ");
+        Console.WriteLine($"amount: {amount} ");
+        Console.WriteLine($"price: {price} ");
+    }
+
+    public static Product operator+(Product p, int val) 
+    {
+        p.Amount += val;
+        return p;
+    }
+
+    public static Product operator -(Product p, int val)
+    {
+        p.Amount -= val;
+        return p;
+    }
+
+    public static bool operator ==(Product p, Product p2)
+    {
+        return p.Price == p2.Price;
+    }
+
+    public static bool operator !=(Product p, Product p2)
+    {
+        return p.Price != p2.Price;
+
+    }
+
+    public static bool operator <(Product p, Product p2)
+    {
+        return p.Amount < p2.Amount;
+
+    }
+
+    public static bool operator >(Product p, Product p2)
+    {
+        return p.Amount > p2.Amount;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,10 +4,28 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        TemperatureArray t = new TemperatureArray();
-        t.Show();
-        t.InputTemp();
-        t.Show();
-        Console.WriteLine($"average: {t.TempAverage()}");
+        //TemperatureArray t = new TemperatureArray();
+        //t.Show();
+        //t.InputTemp();
+        //t.Show();
+        //Console.WriteLine($"average: {t.TempAverage()}");
+
+        Product apple = new Product("Apple", 10, 100);
+        Product banana = new Product("Banana", 8, 50);
+
+        apple.Show();
+        banana.Show();
+
+        apple += 5;
+        apple -= 3;
+        banana -= 8;
+
+        apple.Show();
+        banana.Show();
+
+        Console.WriteLine(apple > banana);
+        Console.WriteLine(apple < banana);
+        Console.WriteLine(apple == banana);
+        Console.WriteLine(apple != banana);
     }
 }
